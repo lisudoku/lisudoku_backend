132c7dc2d19aa5f1f8a85c98b298c6cfeb3be08f
George Marcus <george.marcus922@gmail.com>
2022-11-20 17:43:45 +0200

    Explicitly store default regions

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -5,7 +5,7 @@
 # - solution: the unique solution to the puzzle
 # - tags: TBD
 class Puzzle < ApplicationRecord
-  before_create :set_public_id
+  before_create :set_public_id, :ensure_default_regions
   validate :check_constraints_keys
 
   enum variant: %w[classic killer thermo arrow irregular kropki topbot diagonal mixed].index_by(&:itself)
@@ -19,5 +19,40 @@ class Puzzle < ApplicationRecord
 
   def check_constraints_keys
     errors.add(:constraints, 'constraints does not contain grid_size') unless constraints.has_key?('grid_size')
+  end
+
+  def compute_region_sizes(grid_size)
+    if grid_size == 4
+      [ 2, 2 ]
+    elsif grid_size == 6
+      [ 2, 3 ]
+    else
+      [ 3, 3 ]
+    end
+  end
+
+  def ensure_default_regions
+    return if constraints['regions'].present?
+
+    grid_size = constraints['grid_size']
+    region_height, region_width = compute_region_sizes(grid_size)
+
+    regions = []
+    (grid_size / region_height).times.each do |region_row_index|
+      (grid_size / region_width).times.each do |region_col_index|
+        region = []
+        region_height.times.each do |row_index|
+          region_width.times.each do |col_index|
+            region << {
+              row: region_row_index * region_height + row_index,
+              col: region_col_index * region_width + col_index,
+            }
+          end
+        end
+        regions << region
+      end
+    end
+
+    constraints['regions'] = regions
   end
 end
