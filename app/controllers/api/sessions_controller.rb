48b10b86324b01b8031b38dbe9963362dae86b20
George Marcus <george.marcus922@gmail.com>
2024-02-03 16:53:11 +0200

    Fix login handler

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,17 +2,30 @@ class Api::SessionsController < Devise::SessionsController
   include AuthHelpers
   respond_to :json
 
-  private
-
-  def respond_with(resource, _opts = {})
-    current_user ? login_success(resource) : login_failure(resource)
+  # POST /login
+  def create
+    self.resource = warden.authenticate(auth_options)
+    if self.resource.present?
+      set_flash_message(:notice, :signed_in) if is_navigational_format?
+      sign_in(resource_name, resource)
+      if !session[:return_to].blank?
+        redirect_to session[:return_to]
+        session[:return_to] = nil
+      else
+        login_success(resource)
+      end
+    else
+      login_failure
+    end
   end
+
+  private
 
   def login_success(user)
     render_user(user)
   end
   
-  def login_failure(user)
+  def login_failure
     render json: {
       error: 'Invalid username / password combination',
     }, status: :unauthorized
